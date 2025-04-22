c920d900629905fad81b630819e795f8388408ec
win4abhang <win4abhang@gmail.com>
2025-04-22 11:55:06 +0530

    map accuracy

--- src/components/StoreList.jsx
+++ src/components/StoreList.jsx
@@ -230,7 +230,7 @@ const StoreList = () => {
               onClick={() => {
                 if (store.latitude && store.longitude) {
                   window.open(
-                    `https://www.google.com/maps/dir/?api=1&query=${store.latitude},${store.longitude}&destination_place_id=${store.name}`,
+                    `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(store.name)}@${store.latitude},${store.longitude}`,
                     "_blank"
                   );
                 } else {
